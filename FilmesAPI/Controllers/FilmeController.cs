527ad1fb42ce48ad7ddc56b85f7826d7e501fa78
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2024-10-17 22:07:24 -0300

    Implementada a rota GET da API

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,30 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : Controller
+    {
+        private List<Filme> _filmes;
+
+        public FilmeController()
+        {
+            _filmes = new()
+            {
+                new Filme("10 Coisas Que Eu Odeio Em Você", new DateTime(1999, 3, 31), "Comédia Romântica", new TimeSpan(1, 37, 0), "Gil Junger"),
+                new Filme("Como Perder um Homem Em 10 Dias", new DateTime(2003, 1, 27), "Comédia Romântica", new TimeSpan(1, 56, 0), "Donald Petrie"),
+                new Filme("Mamma Mia!", new DateTime(2008, 9, 12), "Musical", new TimeSpan(1, 48, 0), "Phyllida Lloyd")
+            };
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Obter()
+        {
+            return Ok(_filmes);
+        }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmesAPI.Models
+{
+    public class Filme(string nome, DateTime dataLancamento, string genero, TimeSpan duracao, string nomeDiretor)
+    {
+        public string Nome { get; set; } = nome;
+        public DateTime DataLancamento { get; set; } = dataLancamento;
+        public string Genero { get; set; } = genero;
+        public TimeSpan Duracao { get; set; } = duracao;
+        public string NomeDiretor { get; set; } = nomeDiretor;
+    }
+}
